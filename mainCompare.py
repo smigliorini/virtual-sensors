ede69ef1e1a8e68d892b768fd8118814c17a6af9
Claudio Tomazzoli <claudio.tomazzoli@univr.it>
2024-01-26 18:56:13 +0100

    Analisi di ablazione con e senza shuffle, su tutti e 8 i sensori. Dataset per tutti e 8 i sensori

--- mainCompare.py
+++ mainCompare.py
@@ -1,6 +1,8 @@
 from modelEvaluation import ModelEvaluator
 from hyperParameters import HyperParameters
 from constants import *
+import csv
+import os
 
 SENSORE = 4
 default_time_lag = 7
@@ -18,6 +20,11 @@ validation_split = 0.1
 thisdropout = 0.0
 batch_size = 32
 
+
+def generateCSVFileName(nomeFile):
+    filename = DIR_ESITI + os.path.sep + nomeFile + CSV_EXTENSION
+    return filename
+
 def defineHyperParams(datadescr,time_lag=-1,epochs=-1,shuffle=True):
     hyperparameterValues = {
         BATCH_SIZE_LABEL: batch_size,
@@ -34,45 +41,55 @@ def defineHyperParams(datadescr,time_lag=-1,epochs=-1,shuffle=True):
         hyperparameterValues[TIME_LAG_LABEL] = time_lag
     if epochs > 0:
         hyperparameterValues[EPOCHS_LABEL] = epochs
-
+    if shuffle:
+        hyperparameterValues[BATCH_SIZE_LABEL] = 64
+    else:
+        hyperparameterValues[BATCH_SIZE_LABEL] = 32
     result = HyperParameters(hyperparameterValues)
     return result
 
-def execEval(datadescr='-',time_lag=-1,epochs=-1):
+def execEval(datadescr='-',time_lag=-1,epochs=-1,sensore=SENSORE):
     hyperparameterValues = defineHyperParams(datadescr,time_lag=time_lag,epochs=-epochs,shuffle=True)
-    compare = ModelEvaluator(hyperparameterValues, SENSORE,data_headers.get(datadescr),hyperparameterValues.shuffle)
+    compare = ModelEvaluator(hyperparameterValues, sensore,data_headers.get(datadescr),hyperparameterValues.shuffle)
     sklearn_metrics_mape_shuffle,predicted_shuffle = compare.evaluate()
+    print('sklearn.metrics.mape (shuffle) ', sklearn_metrics_mape_shuffle)
 
     hyperparameterValues.shuffle=False
-    compare = ModelEvaluator(hyperparameterValues, SENSORE, data_headers.get(datadescr),hyperparameterValues.shuffle)
+    compare = ModelEvaluator(hyperparameterValues, sensore, data_headers.get(datadescr),hyperparameterValues.shuffle)
     sklearn_metrics_mape_fromTop, predicted_fromTop = compare.evaluate()
 
-    print('sklearn.metrics.mape (shuffle) ', sklearn_metrics_mape_shuffle)
     print('sklearn.metrics.mape (from top) ', sklearn_metrics_mape_fromTop)
 
     return sklearn_metrics_mape_shuffle,sklearn_metrics_mape_fromTop
 
-def myMain(time_lag=-1,epochs=-1):
+def myMain(time_lag=-1,epochs=-1,sensore=SENSORE):
     result = {}
-    result['base-shuffle'],result['base-fromTop'] = execEval(datadescr='base',time_lag=time_lag,epochs=-epochs)
+    result['base-shuffle'],result['base-fromTop'] = execEval(datadescr='base',time_lag=time_lag,epochs=-epochs,sensore=sensore)
     #
     # out['esteso'] = execEval(datadescr='esteso')
     #
-    result['meno_tempo-shuffle'],result['meno_tempo-fromTop'] = execEval(datadescr='meno_tempo',time_lag=time_lag,epochs=-epochs)
-    result['senza_distanza-shuffle'],result['meno_tempo-fromTop'] = execEval(datadescr='senza_distanza',time_lag=time_lag,epochs=-epochs)
-    result['senza_evja-shuffle'],result['meno_tempo-fromTop'] = execEval(datadescr='senza_evja',time_lag=time_lag,epochs=-epochs)
+    result['meno_tempo-shuffle'],result['meno_tempo-fromTop'] = execEval(datadescr='meno_tempo',time_lag=time_lag,epochs=-epochs,sensore=sensore)
+    result['senza_distanza-shuffle'],result['senza_distanza-fromTop'] = execEval(datadescr='senza_distanza',time_lag=time_lag,epochs=-epochs,sensore=sensore)
+    result['senza_evja-shuffle'],result['senza_evja-fromTop'] = execEval(datadescr='senza_evja',time_lag=time_lag,epochs=-epochs,sensore=sensore)
     return result
 
+def writeToCsvFile(to_csv):
+    outFile = generateCSVFileName('esiti')
+    keys = to_csv[0].keys()
+    with open(outFile, 'w', newline='') as f:
+        dict_writer = csv.DictWriter(f, fieldnames=keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(to_csv)
 
 if __name__ == '__main__':
     print('iniziato')
-    out={}
-    '''
-    for epoch in range(35,55,5):
-        for timestep in range(4,8,1):
-            out = myMain(time_lag=timestep,epochs=epoch)
-            print(str(out))    
-    '''
-    out = myMain(epochs = 400,time_lag = 7)
-    print(str(out))
+    to_csv=[]
+    out = {}
+    for sensore in range(1,9):
+        out = myMain(epochs = 400,time_lag = 7,sensore=sensore)
+        out['sensore']=sensore
+        to_csv.append(out)
+        print(str(out))
+
+    writeToCsvFile(to_csv)
     print('finito')
