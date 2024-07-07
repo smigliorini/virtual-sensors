9522f5959f9eec6e563edf66904ee7de9c0c2409
Claudio Tomazzoli <claudio.tomazzoli@univr.it>
2024-07-07 19:37:49 +0200

    bug, consideravo solo pochi dati per evja

--- CalcoloRMSEedRquadro/calcolaRMSEeRquadro.py
+++ CalcoloRMSEedRquadro/calcolaRMSEeRquadro.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 import csv
 from constants import *
 from sklearn.metrics import r2_score, mean_squared_error
@@ -22,23 +23,31 @@ if __name__ == '__main__':
     for tipoSplit in tipiSplit:
         inFilename =DIR_ESITI + os.path.sep + "mieidaticonpredizione_epochs_400_timesteps_5_"+tipoSplit+".csv"
         outFilename = DIR_ESITI + os.path.sep + "metriche_"+tipoSplit+".csv"
+
         dfDati = pd.read_csv(inFilename)
         result=[]
         for sensore in range(1, 8):
             nomeColTarget = 'target_' + str(sensore)
             nomeColYTEST = 'y_test_' + str(sensore)
             nomeColPredicted = 'predicted_' + str(sensore)
-            nomeColEvja = 'evja_temp_' + str(sensore)
 
             ColTarget = dfDati[nomeColTarget]
             ColYTEST = dfDati[nomeColYTEST]
             ColPredicted = dfDati[nomeColPredicted]
-            ColEvja = dfDati[nomeColEvja]
 
             R2_RNN = r2_score(ColTarget,ColPredicted)
             RMSE_RNN = my_rmse(ColTarget,ColPredicted)
-            R2_EVJA = r2_score(ColTarget, ColEvja)
-            RMSE_EVJA = my_rmse(ColTarget, ColEvja)
+
+            fileNameDatiOrig = DIR_DATI + os.path.sep + "Dataset_original_sens_" + str(sensore) + ".csv"
+            dfOrig = pd.read_csv(fileNameDatiOrig)
+            dfOrigNoNan = dfOrig.replace([np.nan, -np.inf], 0)
+            dfOrigPocheColonne = dfOrigNoNan.loc[:, ["target", "evja_temp"]]
+
+            ColOrig = dfOrigPocheColonne["target"]
+            ColEvja = dfOrigPocheColonne["evja_temp"]
+
+            R2_EVJA = r2_score(ColOrig, ColEvja)
+            RMSE_EVJA = my_rmse(ColOrig, ColEvja)
             rigaEsiti ={"Sensore":str(sensore),"R2_RNN":R2_RNN,"RMSE_RNN":RMSE_RNN,"R2_EVJA":R2_EVJA,"RMSE_EVJA":RMSE_EVJA}
             result.append(rigaEsiti)
         print(result)
